5eddef8df82030671ce470d5b49b232f78028a44
navirars <79823144+navirars@users.noreply.github.com>
2021-12-08 21:57:22 +0700

    new

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import cv2
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = './static/uploads/'
-model = load_model('seledri_jeruknipis_class_model.h5')
+model = load_model('nangka_sirih_class_model.h5')
 
-class_dict = {0: 'Jeruk Nipis (Jeruk Nipis)', 1: 'Seledri (Seledri)'}
+class_dict = {0: 'Daun Nangka (Nangka)', 1: 'Daun Sirih (Sirih)'}
 
 def predict_label(img_path):
     query = cv2.imread(img_path)
